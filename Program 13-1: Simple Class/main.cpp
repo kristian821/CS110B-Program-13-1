fedab6b5ecd2ad1e986cb987a773b214a0245b43
Kristian Mitchell <kmitch79@mail.ccsf.edu>
2025-11-01 16:01:01 -0700

    Initial Commit

---
+++ Program 13-1: Simple Class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Program 13-1: Simple Class
+//
+//  Created by Kristian Mitchell on 11/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
